7614c14675af95a8280aefc14f86eb134c301642
Ihor <stivok1@mail.ru>
2021-11-29 15:12:48 +0300

    add fuct addNewPost

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import './styles/app.css'
 
 function App() {
-  const [posts, setProps] = useState([
+  const [posts, setPost] = useState([
     { id: 1, title: 'JavaScript', body: 'description' },
     { id: 2, title: 'JavaScript', body: 'description' },
     { id: 3, title: 'JavaScript', body: 'description' },
   ])
 
+  const [title, setTitle] = useState('')
+
+  const [body, setBody] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    const newPost = {
+      id: Date.now(),
+      title,
+      body,
+    }
+    setPost([...posts, newPost])
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="App">
-      {posts.map((post )=>
-        <PostItem post={post} key={post.id} />
-      )}
+      <form>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="название поста" />
+        <MyInput
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type="text"
+          placeholder="описание поста" />
+        <MyButton onClick={addNewPost}>Создать Пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов 1" />
     </div>
   );
 }
---
+++ src/components/UI/button/MyButton.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import classes from './MyButton.module.css'
+
+const MyButton = ({children, ...props}) => {
+    return (
+            <button {...props} className={classes.myBtn}>
+                {children}
+            </button>
+    )
+}
+
+export default MyButton
